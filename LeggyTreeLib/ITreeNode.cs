ebd886027347552fe0a7883917b20d9dee894e1d
leigh966 <leigh966@hotmail.com>
2023-12-07 11:51:49 +0000

    Add project files.

---
+++ LeggyTreeLib/ITreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeggyTreeLib
+{
+    public interface ITreeNode<T> : IEnumerable<T>
+    {
+        public List<ITreeNode<T>> children { get; }
+        public ITreeNode<T>? parent { get; }
+        public void AddChild(T child);
+
+    }
+}
---
+++ LeggyTreeLib/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace LeggyTreeLib
+{
+    public class TreeNode<T> : ITreeNode<T>
+    {
+        public List<ITreeNode<T>> children => throw new NotImplementedException();
+
+        public ITreeNode<T>? parent => throw new NotImplementedException();
+
+        public void AddChild(T child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
