739574d58e863e37ccf644c08ad2d68f2cdb5988
leigh966 <leigh966@hotmail.com>
2023-12-07 12:46:56 +0000

    added test to check whether children are successfully being added - currently failing

--- TestLeggyTreeLib/TestTreeNode.cs
+++ TestLeggyTreeLib/TestTreeNode.cs
@@ -1,8 +1,16 @@
+using LeggyTreeLib;
+
 namespace TestLeggyTreeLib
 {
     [TestClass]
     public class TestTreeNode
     {
-       
+        [TestMethod]
+        public void TestAddChildren()
+        {
+            ITreeNode<int> parent = new TreeNode<int>(0);
+            parent.AddChild(1);
+            Assert.AreEqual(1, parent.Children[0].Value);
+        }
     }
 }
