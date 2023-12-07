4a9e3ad8ebd84609334711ee134ce156920e3123
leigh966 <leigh966@hotmail.com>
2023-12-07 12:53:23 +0000

    Added test to check that the parent of the new child is the same as the parent that it was added to - currently failing

--- TestLeggyTreeLib/TestTreeNode.cs
+++ TestLeggyTreeLib/TestTreeNode.cs
@@ -12,5 +12,12 @@ namespace TestLeggyTreeLib
             parent.AddChild(1);
             Assert.AreEqual(1, parent.Children[0].Value);
         }
+        [TestMethod]
+        public void TestParentsAreEqual()
+        {
+            ITreeNode<int> parent = new TreeNode<int>(0);
+            parent.AddChild(1);
+            Assert.AreEqual(parent, parent.Children[0].Parent);
+        }
     }
 }
